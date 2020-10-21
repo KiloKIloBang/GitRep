59cd97c3fad251ba4d596d3b4ed9612c868b46f3
Seymour Talletti <seymourtalletti@gmail.com>
2020-10-21 19:55:34 +0300

    Строка для троля

--- lol/Program.cs
+++ lol/Program.cs
@@ -4,22 +4,26 @@ namespace lol
 {
     class Program
     {
+        static void Text(ref string text)
+        {
+            text = text.Replace("а", "");
+            text = text.Replace("о", "");
+            text = text.Replace("э", "");
+            text = text.Replace("е", "");
+            text = text.Replace("и", "");
+            text = text.Replace("ы", "");
+            text = text.Replace("у", "");
+            text = text.Replace("ё", "");
+            text = text.Replace("ю", "");
+            text = text.Replace("я", "");
+            Console.WriteLine(text);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Введи сообщение глупый троль");
             string message = Console.ReadLine();
-            char[] mes = message.ToCharArray();
-            string fixMessage;
-            for (int q = 0; q < message.Length; q++)
-            {
-                char a = 'а', b = 'о', c = 'э', d = 'е', e = 'и', f = 'ы', g = 'у', k = 'ё', l = 'ю', m = 'я';
-                char t = 't';
-                if (Array.IndexOf<char>(t, char.ToLower(message(q)))) == a | message[q] ==  b | c | d | e | f | g | k | l | m)
-                {
-                    mes[q] = t;
-                }
-
-            }
+            Text(ref message);
+            
         }
     }
 }
