9c6197eb3f5c3e0d8fa680413e2bbaa38d38b453
Seymour Talletti <seymourtalletti@gmail.com>
2020-10-21 19:12:25 +0300

    Recursion

---
+++ Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Recursion(int a)
+        {
+            Console.WriteLine(a%10);
+            a = a / 10;
+            if (a == 0) return;
+            Recursion(a);
+
+        }
+        static void Main(string[] args)
+        {
+            Random numb = new Random();
+            int a = numb.Next();
+            Console.WriteLine(a);
+            Recursion(a);
+        }
+    }
+}
