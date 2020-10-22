f15ab86741ef069163dfb0bd819a0cf12094f02f
Seymour Talletti <seymourtalletti@gmail.com>
2020-10-22 19:31:48 +0300

    Верблюжий Шрифт

---
+++ lol2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace Text
+{
+    class Program
+    {
+
+        static void Impostor(ref string text)
+        {
+            text = text.Replace("!", "");
+            text = text.Replace("?", "");
+            text = text.Replace(".", "");
+            text = text.Replace(",", "");
+            text = text.Replace("'", "");
+            text = text.Replace(":", "");
+            text = text.Replace(";", "");
+
+
+        }
+
+        static void FirstUpper(ref string text)
+        {
+            
+            string temp = "";
+            string[] words = text.Split(' ');
+            for (int q = 0; q < words.Length; q++)
+            {
+                
+                if (words[q].Length > 1)
+                    words[q] = words[q].Substring(0, 1).ToUpper() + words[q].Substring(1, words[q].Length - 1).ToLower();
+                else words[q] = words[q].ToUpper();
+
+                
+                temp = temp + words[q];
+                
+            }
+
+            Console.WriteLine(temp);
+        }
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            Impostor(ref message);
+            FirstUpper(ref message);
+
+
+           
+
+        }
+    }
+}
