bc45a773f585149188a7e357bc3bd076fca71bcf
Seymour Talletti <seymourtalletti@gmail.com>
2020-10-22 21:32:17 +0300

    Верблюжий шрифт 1.2

--- lol2/Program.cs
+++ lol2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Threading;
 
 namespace Text
 {
@@ -15,8 +16,11 @@ namespace Text
             text = text.Replace("'", "");
             text = text.Replace(":", "");
             text = text.Replace(";", "");
-
-
+            text = text.Replace("-", "");
+            text = text.Replace("(", "");
+            text = text.Replace(")", "");
+            text = text.Replace("<", "");
+            text = text.Replace(">", "");
         }
 
         static void FirstUpper(ref string text)
@@ -27,25 +31,29 @@ namespace Text
             for (int q = 0; q < words.Length; q++)
             {
                 
-                if (words[q].Length > 1)
-                    words[q] = words[q].Substring(0, 1).ToUpper() + words[q].Substring(1, words[q].Length - 1).ToLower();
+                if (words[q].Length > 1) words[q] = words[q].Substring(0, 1).ToUpper() + words[q].Substring(1, words[q].Length - 1).ToLower();
                 else words[q] = words[q].ToUpper();
 
                 
                 temp = temp + words[q];
                 
             }
-
+           
             Console.WriteLine(temp);
+            
         }
         static void Main(string[] args)
         {
-            string message = Console.ReadLine();
-            Impostor(ref message);
-            FirstUpper(ref message);
+            do
+            {
+                string message = Console.ReadLine();
+                Console.Clear();
+                Impostor(ref message);
+                FirstUpper(ref message);
+            }
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
 
 
-           
 
         }
     }
